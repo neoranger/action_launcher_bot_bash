e9318743cb72e7af8b868eadac2377bc888f96b8
neoranger <neoranger@disroot.org>
2020-01-23 17:23:54 -0300

    se modificó el script para mandar mensajes por parametros y no hardcodeado

--- prueba_de_mensaje.sh
+++ prueba_de_mensaje.sh
@@ -5,7 +5,7 @@
 for line in $(cat token.txt); do TOKEN="$line" ; done
 
 ID="5482488"
-MENSAJE="Esto es un Mensaje de Prueba"
+#MENSAJE="Esto es un Mensaje de Prueba"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
-curl -s -X POST $URL -d chat_id=$ID -d text="$MENSAJE"
+curl -s -X POST $URL -d chat_id=$ID -d text="$1"
